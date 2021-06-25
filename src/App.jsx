e28fda052e991764899186a959e8a49c87d78d94
Stephanie Freitag <rendaks628@gmail.com>
2021-06-25 13:19:30 +0200

    create addStudent method

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,11 @@ class App extends React.Component {
     });
   }
 
-  /*Write an addStudent method here*/
+  /*Write an addStudent method here*/addStudent(studentName) {
+    this.setState({
+      assignments: this.state.assignments.concat(studentName)
+    });
+  }
 
   addGrade(assignment, student, score) {
     let grades = this.state.grades;
